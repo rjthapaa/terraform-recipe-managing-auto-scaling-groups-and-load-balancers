762ead774fba9b70bcf17b09a341d7efa5914af1
rjthapaa <127729631+rjthapaa@users.noreply.github.com>
2023-07-22 17:01:26 +0530

    Update infrastructure.tf

--- infrastructure.tf
+++ infrastructure.tf
@@ -87,7 +87,7 @@ resource "aws_launch_configuration" "web" {
 
   image_id = "ami-0947d2ba12ee1ff75" # Amazon Linux 2 AMI (HVM), SSD Volume Type
   instance_type = "t2.micro"
-  key_name = "Lenovo T410"
+  key_name = "saikiran"
 
   security_groups = [ aws_security_group.allow_http.id ]
   associate_public_ip_address = true
